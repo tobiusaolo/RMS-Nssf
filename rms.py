2b9f4d6d556c6c0b01e76da8ce911351931487a8
tobiusaolo <tobiusaolo21@gmail.com>
2020-01-07 13:24:53 +0300

    new12

--- rms.py
+++ rms.py
@@ -509,4 +509,4 @@ def Change_Password():
             raise e
     return render_template('settings.html')
 if __name__ == "__main__":
-   app.run(debug=True, host='0.0.0.0', port=5000)
+   app.run()
